24b74df1abdaeec752adf67f7bd8cbb9d90d5263
Bagesree S Sumanan <bagesree.s.sumanan@xminds.com>
2025-10-07 17:44:23 +0530

    remove unwanted code

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,40 +1,6 @@
 const Profile = require('../models/profile');
 const User = require('../models/user');
 
-const createProfile = async (req, res) => {
-    try {
-    if (!req.body) {
-      return res.sendStatus(400);
-    }
- 
-    const newProfile= new Profile(req.body);
-    const savedProfile = await newProfile.save();
- 
-    console.log(savedProfile);
-    return res.status(201).json({ msg: "profile saved successfully", data: savedProfile });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ msg: "Unable to create new profile", error: error.message });
-  }
-};
-const createUser = async (req, res) => {
-    try {
-    if (!req.body) {
-      return res.sendStatus(400);
-    }
- 
-    const newUser= new User(req.body);
-    const savedUser = await newUser.save();
- 
-    console.log(savedUser);
-    return res.status(201).json({ msg: "User saved successfully", data: savedUser });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ msg: "Unable to create new User", error: error.message });
-  }
-};
-
-
 const createUserWithProfile = async(req,res)=>{
     try {
     const { name, email, bio, website } = req.body;
