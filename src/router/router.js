25e16f616d3db94a19ce24812b11cc616650a24d
Bagesree S Sumanan <bagesree.s.sumanan@xminds.com>
2025-10-09 11:43:13 +0530

    validator added

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {createUserWithProfile,UpdateUserandProfile,DeleteUserandprofile}= require('../controller/controller');
+const { userValidationRules, validate } = require("../validation/validator");
 
-router.post("/createUserWithProfile", createUserWithProfile);
+router.post("/createUserWithProfile", userValidationRules(),validate,createUserWithProfile);
 router.put("/UpdateUserandProfile/:id", UpdateUserandProfile);
 router.delete("/DeleteUserandprofile/:id", DeleteUserandprofile);
 module.exports = router;
---
+++ src/validation/validator.js
@@ -0,0 +1,27 @@
+const { body, validationResult } = require("express-validator");
+const userValidationRules = () => {
+  return [
+    // username must be an email
+    body('email').isEmail().withMessage('Must be a valid email').normalizeEmail(),
+    // bio must be at least 5 chars long
+    body("bio").isLength({ min: 6 }).withMessage('bio must be at least 6 characters'),
+  ];
+};
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  const extractedErrors = [];
+  errors.array().map((err) => extractedErrors.push({ [err.path]: err.msg }));
+ 
+  return res.status(422).json({
+    errors: extractedErrors,
+  });
+};
+
+module.exports = {
+  userValidationRules,
+  validate,
+};
